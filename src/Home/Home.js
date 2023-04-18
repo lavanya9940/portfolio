2b32604143005c6eaa328dc1e943183167ce6b14
Lavanya Pavada <lavanyapavada.ce@gmail.com>
2023-04-18 20:03:45 +0530

    changes made in home.js

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,7 +3,7 @@ import React,{Component} from "react";
 export class Home extends Component{
     render(){
         return(
-            <div>home comp</div>
+            <div>home comp is created </div>
         );
     }
 }
